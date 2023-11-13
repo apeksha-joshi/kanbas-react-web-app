35d3b40a3ccc04fd2bda100084d3c91dc411968a
apekshasj <joshi.ape@northeastern.edu>
2023-11-13 16:01:11 -0500

    Update httpClientConfig.js

--- src/Labs/a5/httpClientConfig.js
+++ src/Labs/a5/httpClientConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 //axios.defaults.baseURL = "http://localhost:4000/api/";
 
-axios.defaults.baseURL = "https://kanbas-node-server-app-viv0.onrender.com";
-//process.env.REACT_APP_API_BASE;
+//axios.defaults.baseURL = "https://kanbas-node-server-app-viv0.onrender.com";
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE;
 
 
 export default axios;
