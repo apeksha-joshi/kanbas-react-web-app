e43ca8621cae2b6fa0946c89623c885f05071f0a
apekshasj <joshi.ape@northeastern.edu>
2023-11-13 12:36:43 -0500

    minor change

--- src/Kanbas/Services/index.js
+++ src/Kanbas/Services/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //axios.defaults.baseURL = "http://localhost:4000/api/";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_BASE;;
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE;
 
 
 export default axios;
