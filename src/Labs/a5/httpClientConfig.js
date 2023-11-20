80cc8cf5fc8250d46ae4b894b7a5be0ae0ebd833
apekshasj <joshi.ape@northeastern.edu>
2023-11-20 18:38:40 -0500

    minor fixes

--- src/Labs/a5/httpClientConfig.js
+++ src/Labs/a5/httpClientConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-//axios.defaults.baseURL = "http://localhost:4000/api/";
+//axios.defaults.baseURL = "http://localhost:4000";
 
 //axios.defaults.baseURL = "https://kanbas-node-server-app-viv0.onrender.com";
 
